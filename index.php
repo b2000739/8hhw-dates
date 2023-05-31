40e5b1cc6296148952f404c718eacb6bdd9e1861
b2000739 <113001869+b2000739@users.noreply.github.com>
2023-05-31 21:48:00 +0600

    рш

--- index.php
+++ index.php
@@ -1,49 +1,54 @@
 <?php
-$week = array('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
-echo $week[date('w', '06.06.2006')];
-echo $week[date('w', '14.07.2008')];
+$week = array('Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота',);
+
+echo $week[date('w')];
+echo $week[date('w', mktime(0, 0, 0, 6, 6, 2006))];
+echo $week[date('w', mktime(0, 0, 0, 7, 14, 2008))];
 $months = array(1 => 'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь');
 echo '<br>';
 echo $months[date('n')];
 echo '<br> в текущем месяце: ' . date('t') . ' дней';
 echo '<br>';
-$date = new DateTime('14-05-20');
-$date->modify('+2 day +1 month +3 day +1 year -3 day');
-
-$results = $date->format('Y-m-d H:i:s');
-
-if ($results) {
-	echo ($results);
-} else {
-	echo ('Formatting failed');
+?>
+<form method="post" enctype='multipart/form-data'>
+	<p>Выберите год<input type='text' name='year'></p>
+	<input type='submit' value="Сохранить" name='ok'>
+</form>
+<?
+if ($_POST['ok']) {
+	$year = $_POST['year'];
+	if (date("L", mktime(0, 0, 0, 7, 7, $year)) == 1) {
+		echo "Ваш год високосный";
+	} else {
+		echo 'Ваш год не високосный';
+	}
+	echo '<br>';
 }
+
+$date = new DateTime('2025-12-31');
+$date->modify('+ 2 day + 1 month + 3 day + 1 year - 3 day');
+$results = $date->format('Y-m-d');
+echo $results;
 echo '<br>';
-$today = new Datetime(date('m-d-Y'));
-$New_year = new DateTime(date("m-d-Y", mktime(0, 0, 0, 1, 1, date("Y") + 1)));
-$interval = $New_year->diff($today);
-$results = $New_year->format('%S:%I:%H');
+$months = array(1 => 'января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря');
+$week = array(1 => 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
 
-if ($results) {
-	echo ($results);
-} else {
-	echo ('Formatting failed');
+$ny = mktime(0, 0, 0, 01, 01, date('Y') + 1);
+$timeUntilNY = $ny - time();
+$days = floor($timeUntilNY / 3600 / 24);
+echo $days;
+// $results = $New_year->format('%S:%I:%H');\
+$date = new DateTime(date('Y-m-d'));
+$until = date('w') - 1;
+$until_b = $until + 1;
+$date->modify('+' . -$until_b . ' day');
+echo '<br> <br>';
+for ($i = 1; $i < 8; $i++) {
+	$date->modify('+ 1 day');
+	$month = $date;
+	$menth = $month->format('n');
+	$results = $date->format($week[$i] . ', j ' . $months[$menth] . ' Y года');
+	echo '<br>';
+	echo $results;
+	echo '<br><br>';
 }
-//ЭТО ТОЖЕ НЕ РАБОТАЕТ
-
-// echo ($_POST);
-// if (date($_POST) == 1) {
-// 	echo 'ваш год весокосен';
-// } elseif (date($_POST) == 0) {
-// 	echo 'ваш год не весокосен';
-// } else {
-// 	echo "Введите год";
-// }
-
-//
-// <form action="index.php" method="post">
-// 	<input type="string" class="input">
-// 	<input type="button" value="отправить" type="submit">
-// </form>
-
-
-// НЕ РАБОТАЕТ НУЖНА ПОМОЩЬ
